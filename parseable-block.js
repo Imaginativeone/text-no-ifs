7c4fb2dbb9bb6d05ba861006cab39c4ff126431f
Doug Franklin <doug@dougfranklin.com>
2020-10-07 17:12:52 -0400

    Updated

--- parseable-block.js
+++ parseable-block.js
@@ -1,45 +1,87 @@
-const textdata = {
-
-  will_be_parsed: `**Data** The quick brown fox jumped over the lazy dog! -|-`,
-  will_not_be_parsed: 
-    `
-    The quick brown fox jumped over the lazy dog.
-    `,  
-}
-
+const textdata = `**Data** The quick brown fox jumped over the lazy dog. -|-`;
 const showText = document.getElementById('texttoshow');
-const showString = (input) => {
-  showText.innerHTML = "Text: " + textdata.will_be_parsed;
-}
-
-const parseableText = document.getElementById('parseableText');
-parseableText.value = 'Hello';
-
-console.log(parseableText);
 
 const begMarker = '**Data**';
 const ignMarker = '!';
 const endMarker = '-|-';
 
-function parseBlock(searchStringA, searchStringB, searchStringC) {
+function canParse(iText) {
 
-  const begMarkerFound = textdata.will_be_parsed.indexOf(searchStringA) != -1;
-  const endMarkerFound = textdata.will_be_parsed.indexOf(searchStringB) != -1;
-  const ignMarkerFound = textdata.will_be_parsed.indexOf(searchStringC) != -1;
+  let objParse = {};
+  const markers = ['**Data**', '-|-', '!'];
+  const indexReplacements = ['beg', 'end', 'off'];
 
-  console.log(begMarkerFound);
+  const parseText = markers.filter((marker, i) => {
 
-  if (begMarkerFound && endMarkerFound && !ignMarkerFound) {
-    alert(`${ searchStringA } Found and ${ searchStringB } Found`);
-  } else {
+    // console.log(i);
+    // console.log(`I want to replace ${ marker } with ${ indexReplacements[i] }`);
+    replacedMarker = indexReplacements[i];
+
+    // console.log(`replacedMarker: ${ replacedMarker }`);
+    // console.log(`In text: ${ iText.indexOf(marker) }`);
+    objParse[replacedMarker] = iText.indexOf(marker) + 1;
     
-    alert(`
-      begMarkerFound: ${ begMarkerFound }
-      endMarkerFound: ${ endMarkerFound }
-      ignMarkerFound: ${ ignMarkerFound }
-    `);
-  }
-  
+  });
+
+  return objParse;
+
 }
 
-parseBlock(begMarker, endMarker, ignMarker);
+const can = canParse(textdata);
+// console.log(canParse(textdata));
+
+function parseBlock(objCanParse) {
+  
+  console.log(objCanParse);
+
+  let objConstructed = {}
+
+  console.log(objConstructed);
+
+  const parser = Object.entries(objCanParse).filter((accumulator, value, i) => {
+
+    parsingStates = [-1, -1,  1];
+
+    // accumulator[value[0]] = value[1];
+
+    objConstructed[value[0]] = value[1];
+    console.log('objConstructed', objConstructed, `parsingStates[${ i }]`, parsingStates[i]);
+
+    return addParsingState(objConstructed, objConstructed[value[0]]);
+
+    function addParsingState(object, s) {
+
+      console.log('object', object, 's', s);
+
+      const states = {
+        parse: 'Parse',
+        dontparse: 'Don\'t Parse'
+      }
+    }
+
+    // if (value[0] == "beg" && value[1] !== -1) {
+
+    //   const { end, off, ...begChopped } = objCanParse;
+    //   console.log('Destructured', begChopped);
+      
+    // } else if (value[0] == "end" && value[1] !== -1) {
+
+    //   const { beg, off, ...endChopped } = objCanParse;
+    //   console.log('Destructured', endChopped);
+
+    // } else if (value[0] == "off" && value[1] === -1) {
+
+    //   const { beg, end, ...offChopped } = objCanParse;
+    //   console.log('Destructured', offChopped);
+
+    // } else {
+    //   console.log('Don\'t parse the text');
+    // }
+
+    // return accumulator += value;
+
+  }, accumulator = {});
+
+}
+
+parseBlock(can);
